653dd8966b7b5b4e2844fbf1578a6f5a1246d456
Mekeih-MSU <144586564+Mekeih-MSU@users.noreply.github.com>
2023-10-16 16:15:29 -0400

    Update PGP_APP_ALGO.py

--- PGP_APP_ALGO.py
+++ PGP_APP_ALGO.py
@@ -1,54 +1,82 @@
 import os
 import gnupg
+import pickle
 
-gpg = gnupg.GPG()
+GPG = gnupg.GPG()
+PERSONAL_KEY_FILE = "PERSONAL_KEYS.pkl"
+CONTACTS_KEY_FILE = "CONTACTS_KEYS.pkl"
+
+# FILE I/O ------------------------------->
+def save_data(data, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
+
+def load_data(filename):
+    try:
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
+    except FileNotFoundError:
+        return []
+
+def create_contact(first_name, last_name, phone_number, email, public_key):
+	contact_entry = {
+		"First Name": first_name,
+		"Last Name": last_name,
+		"Phone Number": phone_number,
+		"Email": email,
+		"Public Key": public_key,
+	}
+
+	add_contact(contact_entry, CONTACTS_KEY_FILE)
+
+def add_entry(contact_entry, file):
+	data = load_data(file)
+	data.append(contact_entry)
+	save_data(data, file)
+
+def delete_entry(index, file):
+	data = load_data(file)
+	if (0 <= index < len(data)):
+		data.pop(index)
+		save_data(data, file)
+
+
+# PGP ALGO ------------------------------->
 
 def create_asymmetric_key():
-	input_data = gpg.gen_key_input(
-		name_real='FirstName LastName',
-		name_comment="123-456-7890",
-	    name_email='youremail@example.com',
-	    key_type='RSA'
-	)
-
-	return gpg.gen_key(input_data)
+	input_data = GPG.gen_key_input(key_type='RSA')
+	return GPG.gen_key(input_data)
 
 def get_public_key(key):
-	return gpg.export_keys(key.fingerprint)
+	return GPG.export_keys(key.fingerprint)
 
 def get_private_key(key):
-	return gpg.export_keys(key.fingerprint, True)
-
-def get_name(key):
-	#to-do
-	print(1)
-
-def get_phone_number(key):
-	#to-do
-	print(1)
-
-def get_email(key):
-	#to-do
-	print(1)
+	return GPG.export_keys(key.fingerprint, True)
 
 def encrypt_text(plaintext, recipient_key):
-	encrypted_data = gpg.encrypt(plaintext, recipient_key, always_trust=True)
+	encrypted_data = GPG.encrypt(plaintext, recipient_key, always_trust=True)
 	if encrypted_data.ok:
 		return str(encrypted_data)
 	else:
 		raise Exception(f"Encryption failed: {encrypted_data.status}")
 
 def decrypt_text(encrypted_text):
-    decrypted_data = gpg.decrypt(encrypted_text)
+    decrypted_data = GPG.decrypt(encrypted_text)
     if decrypted_data.ok:
     	return str(decrypted_data)
     else:
     	raise Exception(f"Decryption failed: {decrypted_data.status}")
 
+
+# TEST GROUNDS ------------------------------->
+
+"""
 key = create_asymmetric_key()
+
 public_key = get_public_key(key)
-private_key = get_private_key(key)
 print(public_key)
+
+private_key = get_private_key(key)
 print(private_key)
 print("")
 
@@ -59,3 +87,26 @@ de_t = decrypt_text(en_t)
 print(text)
 print(en_t)
 print(de_t)
+"""
+
+def print_page(file):
+	data = load_data(file)
+	for i in range(len(data)):
+		print(f"#{i}: {data[i]}")
+
+"""
+create_contact(
+	first_name="Mekeih", 
+	last_name="Nelson", 
+	phone_number="12345", 
+	email="test@email.com", 
+	public_key="pub_key",
+	)
+"""
+print("Before Delete")
+print_page(CONTACTS_KEY_FILE)
+print("")
+
+print("After Delete")
+delete_contact(0)
+print_page(CONTACTS_KEY_FILE)
