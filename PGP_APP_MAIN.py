5f4d7bf18fe1117b94a6811a70f7187034ebea3f
Mekeih-MSU <144586564+Mekeih-MSU@users.noreply.github.com>
2023-11-01 06:42:15 -0400

    Update PGP_APP_MAIN.py

--- PGP_APP_MAIN.py
+++ PGP_APP_MAIN.py
@@ -5,6 +5,9 @@ from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTi
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
 import PGP_APP_GUI
+import PGP_APP_ADD_CONTACT_FORM_RUNNER
+import PGP_APP_ADD_KEY_FORM_RUNNER
+import PGP_APP_ALGO
 
 class PGPApp(QtWidgets.QMainWindow, PGP_APP_GUI.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -19,6 +22,17 @@ class PGPApp(QtWidgets.QMainWindow, PGP_APP_GUI.Ui_MainWindow):
 
 		# Remove Default Window Box
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # Set Drop Shadow Window
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 200))
+
+        # Appy Drop Shadow to Frame
+        self.drop_window_frame.setGraphicsEffect(self.shadow)
 
 		# Move Window Function
         def moveWindow(event):
@@ -31,14 +45,29 @@ class PGPApp(QtWidgets.QMainWindow, PGP_APP_GUI.Ui_MainWindow):
         
         # Personal Keys Page Button
         self.personal_btn_page.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.personal_key_page))
+        self.add_key_btn.clicked.connect(lambda: PGP_APP_ADD_KEY_FORM_RUNNER.KeyPage())
 
         # Contact Keys Page Button
         self.contacts_btn_page.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.contact_key_page))
+        self.add_contact_btn.clicked.connect(lambda: PGP_APP_ADD_CONTACT_FORM_RUNNER.ContactPage())
+        self.loadContactPage()
 
         self.show()
     
     def mousePressEvent(self, event):
         self.dragPos = event.globalPos()
+    
+    def loadContactPage(self):
+        data = PGP_APP_ALGO.load_data("CONTACTS_KEYS.pkl")
+        row = 0
+        self.contact_table.setRowCount(len(data))
+        for entry in data:
+            self.contact_table.setItem(row, 0, QtWidgets.QTableWidgetItem(data["First Name"]))
+            self.contact_table.setItem(row, 1, QtWidgets.QTableWidgetItem(data["Last Name"]))
+            self.contact_table.setItem(row, 2, QtWidgets.QTableWidgetItem(data["Phone Number"]))
+            self.contact_table.setItem(row, 3, QtWidgets.QTableWidgetItem(data["Email"]))
+            self.contact_table.setItem(row, 4, QtWidgets.QTableWidgetItem(data["Public Key"]))
+            row += 1
 
 def main():
     app = QApplication(sys.argv)
